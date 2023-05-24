78ab491926dbbe2017e6b8db105f05dededbf995
TACQ <tiagoalexandrequintino@gmail.com>
2023-05-24 19:55:49 +0100

    Correção de alguns bugs na pagina parterns.pph

---
+++ TiagoQuintino/about.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/navbar.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/about.css">
+    <title>Team</title>
+</head>
+
+<body>
+    <header id="header" class="fixed-top header-transparent">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                <h1 class="text-light"><a href="index.html"><span>SurfNautica</span></a></h1>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+                    <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                    <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+                    <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                    <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Drop Down 1</a></li>
+                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                <ul>
+                                    <li><a href="#">Deep Drop Down 1</a></li>
+                                    <li><a href="#">Deep Drop Down 2</a></li>
+                                    <li><a href="#">Deep Drop Down 3</a></li>
+                                    <li><a href="#">Deep Drop Down 4</a></li>
+                                    <li><a href="#">Deep Drop Down 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Drop Down 2</a></li>
+                            <li><a href="#">Drop Down 3</a></li>
+                            <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+
+        </div>
+    </header>
+
+    <div class="presentation">
+
+        <div class="responsive-container-block outer-container justify-content-center align-items-center">
+            <div class="card ">
+                <img src="img/team.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title fs-1 fw-bold">Quem somos?</h5>
+                    <p class="card-text fs-2">Atualmente somos estudantes no Politécnico de Leiria a tirar um TeSP desenvolvimento web e multimédia</p>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="row mt-md-5">
+        <div class="col-lg-4">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+
+            <h2>Parceiro</h2>
+            <p>descricao do Parceiro.</p>
+            <p><a id="btn" class="btn btn-secondary" href="#">Visitar »</a></p>
+        </div>
+        <div class="col-lg-4">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+
+            <h2>Parceiro</h2>
+            <p>descricao do Parceiro.</p>
+            <p><a class="btn btn-secondary" href="#">Visitar »</a></p>
+        </div>
+        <div class="col-lg-4">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+
+            <h2>Parceiro</h2>
+            <p>descricao do Parceiro.</p>
+            <p><a class="btn btn-secondary" href="#">Visitar »</a></p>
+        </div>
+    </div>
+</body>
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-6">
+                </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+
+                    <a href="index.html"><img src="assets/img/favicon.png" alt="" height="100px"></a>
+
+                    <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="mailto:" target="_blank" class="google-plus"><i class="bx bx-envelope"></i></a>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Informações</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Dias Abertos</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Aulas</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Notícias</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Outros Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Sobre</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Parcerias</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Contacto</a></li>
+                    </ul>
+                </div>
+                <div class="credits">
+                    &copy; Copyright <strong><span>SURFNAUTICA</span></strong>. All
+                    Rights
+                    Reserved
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+<script src="js/bootstrap.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/js/main.js"></script>
+
+</html>
---
+++ TiagoQuintino/partenrs.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+<?php
+require('database.php');
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/parterns.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/navbar.css" rel="stylesheet">
+    <title>Parcerias</title>
+</head>
+<title>Parcerias</title>
+</head>
+
+<body>
+
+    <header id="header" class="fixed-top ">
+        <?php
+        require_once 'navbar.php';
+        ?>
+    </header>
+    <div class="container marketing">
+        <div class="row mt-md-5">
+            <?php
+            $sql = "select nome, img, descricao from parcerias;";
+            $result = mysqli_query($connection, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    foreach ($row as $res => $key) {
+                        $name = $row['nome'];
+                        $pic = $row['img'];
+                        $descri = $row['descricao'];
+                    }
+                    echo ("  <div class='col-lg-4'>
+                <img class='bd-placeholder-img rounded-circle' src='assets/img/" . $pic . "' alt='Image' width='140' height='140'> </img>
+                <h2>" . $name . "</h2>
+                <p>" . $descri . "</p>
+            </div>");
+                }
+            } else {
+                echo ("Erro ao executar o select:" . mysqli_connect_error($connection));
+            }
+
+            mysqli_close($connection);
+            ?>
+
+
+            <!--  <div class="row mt-md-5">
+            <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" src="img/<?php /* echo ($pic); */ ?>" alt="Image" width="140" height="140"> </img>
+                <h2><? php/*  echo ($name); */ ?></h2>
+                <p><?php /* echo ($descri); */ ?></p>
+            </div>
+
+            <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" src="img/<?php /* echo ($pic); */ ?>" alt="Image" width="140" height="140"> </img>
+                <h2><? php/*  echo ($name); */ ?></h2>
+                <p><?php /* echo ($descri); */ ?></p>
+            </div>
+
+            <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" src="img/<?php /* echo ($pic); */ ?>" alt="Image" width="140" height="140"> </img>
+                <h2><? php/*  echo ($name); */ ?></h2>
+                <p><?php /* echo ($descri); */ ?></p>
+            </div>
+         </div> -->
+        </div>
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <!-- Nova Parceira com IPL em que os estudantes tem desconto de 10% a 15% + imagem do IPL  -->
+                <h2 class="featurette-heading">Novo Parceiro. <span class="text-muted">IPL.</span></h2>
+                <p class="lead">descricao do Parceiro.</p>
+                <p><a class="btn btn-secondary" href="#">Visitar &raquo;</a></p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                </svg>
+
+            </div>
+        </div>
+
+
+        <hr class="featurette-divider">
+    </div>
+
+    <?php
+    include_once("footer.php");
+    ?>
+
+
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/jquery-3.6.3.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
