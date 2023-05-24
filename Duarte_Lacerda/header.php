6a1ab3a9c097b914e348209519f747914e61a2e6
Portátil Duarte <duartelacerda4@gmail.com>
2023-05-24 10:23:39 +0100

    Footer/Header fix

---
+++ Duarte_Lacerda/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top header-transparent">
+        <div class="container d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <h1 class="text-light"><a href="index.php"><img src="img/favicon.png" alt="" class="img-fluid"><span>SurfNautica</span></h1></a>
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li class="dropdown"><a href="#" class="nav-link scrollto"><span>Sobre</span>
+                            <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">História do Clube</a></li>
+                            <li><a href="#">Parcerias</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#" class="nav-link scrollto"><span>Escola
+                                de Surf</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Dias Abertos</a></li>
+                            <li><a href="#">Aulas</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#" class="nav-link scrollto"><span>Info
+                                Surf</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Pranchas</a></li>
+                            <li><a href="equipamentos.php">Equipamentos</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="#">Noticias</a></li>
+                    <li><a class="nav-link scrollto" href="contacto.php">Contactos</a></li>
+                    <li><a class="nav-link scrollto" href="#">Login</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+
+        </div>
+    </header><!-- End Header -->
+</body>
+
+</html>
